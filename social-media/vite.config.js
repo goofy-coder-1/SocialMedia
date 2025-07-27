1cc2116cb0c8ffba574b6b5e139657c923c9d933
Ujjwal Rana <ujjwolrana2332@gmail.com>
2025-07-27 19:36:55 +0545

    Update vite base path

--- social-media/vite.config.js
+++ social-media/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/SocialMedia/", // 👈 this must match your GitHub repo name exactly
   plugins: [react()],
-})
+});
