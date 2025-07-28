e2895eabef6f28a41e9aa2ed39299e4a327debdf
Ujjwal Rana <ujjwolrana2332@gmail.com>
2025-07-28 08:14:10 +0545

    some changes

--- social-media/vite.config.js
+++ social-media/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/SocialMedia/", // 👈 this must match your GitHub repo name exactly
+  base: "/", // 👈 this must match your GitHub repo name exactly
   plugins: [react()],
 });
